40b5c2e2da37a6a0c08a4731fe161b451e388bf3
TTA0851237295 <kingofzing2004@gmail.com>
2025-06-17 20:41:18 +0700

    Trang chu

---
+++ js/courses.js
@@ -0,0 +1,68 @@
+const courses = [
+            {
+                id: 1,
+                title: "Lập trình C++ cơ bản, nâng cao",
+                originalPrice: "3.500.000",
+                currentPrice: "3.000.000",
+                instructor: "TTA",
+                duration: "3 tháng",
+                image: "img/cpp.png", // Đường dẫn đến ảnh C++
+                displayText: "C++",
+                subtitle: "Từ cơ bản đến nâng cao"
+            },
+            {
+                id: 2,
+                title: "Lập trình React JS",
+                originalPrice: "2.500.000",
+                currentPrice: "2.000.000",
+                instructor: "PTA",
+                duration: "6 tháng",
+                image: "img/react.png", // Đường dẫn đến ảnh React
+                displayText: "React JS",
+                subtitle: "Learn once, write anywhere"
+            },
+            {
+                id: 3,
+                title: "Lập trình JavaScript cơ bản",
+                originalPrice: "1.800.000",
+                currentPrice: "1.500.000",
+                instructor: "HMD",
+                duration: "4 tháng",
+                image: "img/jscoban.png", // Đường dẫn đến ảnh JS cơ bản
+                displayText: "JavaScript",
+                subtitle: "{Cơ bản}"
+            },
+            {
+                id: 4,
+                title: "Lập trình Node & Express.JS",
+                originalPrice: "2.500.000",
+                currentPrice: "2.000.000",
+                instructor: "NQS",
+                duration: "6 tháng",
+                image: "img/node.png", // Đường dẫn đến ảnh Node.js
+                displayText: "Node & Express.JS",
+                subtitle: ""
+            },
+            {
+                id: 5,
+                title: "Lập trình JavaScript nâng cao",
+                originalPrice: "3.500.000",
+                currentPrice: "3.000.000",
+                instructor: "TTA",
+                duration: "6 tháng",
+                image: "img/jsnangcao.png", // Đường dẫn đến ảnh JS nâng cao
+                displayText: "JavaScript",
+                subtitle: "{Nâng cao}"
+            },
+            {
+                id: 6,
+                title: "Nền tảng Web",
+                originalPrice: "1.500.000",
+                currentPrice: "1.000.000",
+                instructor: "TTA",
+                duration: "3 tháng",
+                image: "img/htmlcss.png", // Đường dẫn đến ảnh HTML/CSS
+                displayText: "HTML, CSS",
+                subtitle: "từ zero đến hero"
+            }
+        ];
